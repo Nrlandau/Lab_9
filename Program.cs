113b7da09af381410a6787033eb55f777784015b
Nicholas Landau <nrlandau@gmail.com>
2018-10-17 15:48:34 -0400

    Created main input loop

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Lab9
 {
@@ -42,6 +43,7 @@ namespace Lab9
             }
             return personInfo;
         }
+        
         static void DisplayAllPeopleAndInfo(List<Hashtable> AllInfo)
         {
             for(int key = 1; key < AllInfo[0].Count + 1; key++)
@@ -52,6 +54,61 @@ namespace Lab9
                 }
                 System.Console.WriteLine();
             }
+        }
+        static bool Loop(List<Hashtable> data)
+        {
+            int keyNum = 0;
+            string whatInfo;
+            while(true)
+            {
+                try
+                {
+                    System.Console.WriteLine("Input a number(1-{0})",data[0].Count);
+                    keyNum = int.Parse(Console.ReadLine());
+                    if(keyNum >= 1 || keyNum <= data[0].Count)
+                        break;
+                    System.Console.WriteLine("That Data does not exist");
+
+                }
+                catch(FormatException)
+                {
+                    System.Console.WriteLine("That is not a number");
+                }
+            }
+            while(true)
+            {
+                System.Console.WriteLine("{0} is {1}, what do you want to know? ({2} / {3})",keyNum,data[0][keyNum],"Hometown","Favorite Food");
+                whatInfo = Console.ReadLine();
+                if(Regex.IsMatch(whatInfo.ToLower(),"home"))
+                {
+                    System.Console.WriteLine("{0}'s hometown is {1}",data[0][keyNum],data[1][keyNum]);
+                }
+                else if(Regex.IsMatch(whatInfo.ToLower(),"food"))
+                {
+                    System.Console.WriteLine("{0}'s favorite food is {1}",data[0][keyNum],data[2][keyNum]);
+                }
+                else
+                {
+                    System.Console.WriteLine("Sorry, I do not have that data about {0}",data[0][keyNum]);
+                }
+                whatInfo = "";
+                while(!Regex.IsMatch(whatInfo,"^[yYNn]"))
+                {
+                    System.Console.WriteLine("Do you want to know more about {0}?(Yes/No)",data[0][keyNum]);
+                    whatInfo = Console.ReadLine();
+                }
+                if(Regex.IsMatch(whatInfo,"^[nN]"))
+                    break;
+            }
+            whatInfo = "";
+            while(!Regex.IsMatch(whatInfo,"^[yYNn]"))
+            {
+                System.Console.WriteLine("Do you want to know about another student?(Yes/No)");
+                whatInfo = Console.ReadLine();
+            }
+            if(Regex.IsMatch(whatInfo,"^[nN]"))
+                return false;
+            return true;
         }
 
         static void Main(string[] args)
@@ -64,7 +121,7 @@ namespace Lab9
             }
             catch (FileNotFoundException)
             {
-                File = new StreamReader(Console.OpenStandardInput());
+                File = new StreamReader(Console.OpenStandardInput()); // This doesn't work right now
                 System.Console.WriteLine("File Does not exist, Using Console input.");
             }
             try{
@@ -78,13 +135,14 @@ namespace Lab9
                     System.Console.WriteLine("No Info Loaded");
                     throw new EndOfStreamException();
                 }
-                
             }
             finally
             {
                File.Close(); 
             }
-            DisplayAllPeopleAndInfo(people);
+            //DisplayAllPeopleAndInfo(people);
+            while (Loop(people));
+
         }
     }
 }
